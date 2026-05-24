353504b4ebce1a2b305af78cfe37140ed19fc3a6
Lincoln Taranto <54151306+lincolntaranto@users.noreply.github.com>
2026-05-24 07:34:52 -0300

    chore: configura o __init__ dos models

---
+++ API/sistema-escola/models/__init__.py
@@ -0,0 +1,13 @@
+from .aluno import Aluno
+from .cargo import Cargo
+from .nota import Nota
+from .turma import Turma
+from .usuario import Usuario
+
+__all__ = [
+    "Aluno",
+    "Cargo",
+    "Nota",
+    "Turma",
+    "Usuario"
+]
