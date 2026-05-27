a7e951150f8800fed89ab8027fe912a28623a8aa
Lincoln Taranto <54151306+lincolntaranto@users.noreply.github.com>
2026-05-27 07:02:51 -0300

    chore: configura a secret_key

--- API/sistema-escola/main.py
+++ API/sistema-escola/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 app = FastAPI()
 
 from routes.auth_routes import auth_router
