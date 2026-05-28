70aa247b4ebedf283a144dcd06502a5481e3bbfc
Lincoln Taranto <54151306+lincolntaranto@users.noreply.github.com>
2026-05-28 03:54:05 -0300

    feat: cria a funcao verificar_token

--- API/sistema-escola/security.py
+++ API/sistema-escola/security.py
@@ -1,11 +1,16 @@
 import bcrypt
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
 
-from main import ACESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
+from config import ACESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, oauth2_schema
 from models import Usuario
 
 from jose import jwt, JWTError
 
 from datetime import datetime, timedelta, timezone
+
+from models.session import get_session
+
 
 def hash_senha(senha: str) -> str:
     return bcrypt.hashpw(senha.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
@@ -18,11 +23,22 @@ ALGORITHM = "HS256"
 def criar_token(id_usuario, duracao_token=timedelta(int(ACESS_TOKEN_EXPIRE_MINUTES))):
     data_expiracao = datetime.now(timezone.utc) + duracao_token
     dic_info = {
-        "sub" : id_usuario,
+        "sub" : str(id_usuario),
         "exp" : data_expiracao
     }
     jwt_encoded = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
     return jwt_encoded
+
+def verificar_token(token: str = Depends(oauth2_schema), session: Session = Depends(get_session)):
+    try:
+        dict_info = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        id_usuario = int(dict_info.get("sub"))
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Acesso negado!")
+    usuario = session.query(Usuario).filter(Usuario.id==id_usuario).first()
+    if not usuario:
+        raise HTTPException(status_code=401, detail="Acesso invalido!")
+    return usuario
 
 def autenticar_usuario(email, senha, session):
     usuario = session.query(Usuario).filter(Usuario.email==email).first()
